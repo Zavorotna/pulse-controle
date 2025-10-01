f2dcb3e066844cb3479e6a51a051108396eca5e8
Zavorotna <zavorotnaa96@gmail.com>
2025-10-01 11:39:43 +0300

    add page dovidky

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -11,20 +11,21 @@ document.addEventListener("DOMContentLoaded", function () {
             burger.style.display = "none"
             cancel.style.display = "block"
         })
+
         function cancelBurger() {
             menu.style.top = "-110%"
             burger.style.display = "block"
             cancel.style.display = "none"
-            
+
         }
         cancel.addEventListener("click", cancelBurger)
-        
+
         links.forEach(item => {
-            item.addEventListener("click", function(){
+            item.addEventListener("click", function () {
                 cancelBurger()
                 burger.style.display = "none"
             })
-            
+
         })
     }
 
@@ -71,84 +72,125 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         })
     }
+    if (document.querySelector("select")) {
 
-    const typeWrapper = document.getElementById("type-select"),
-        serviceWrapper = document.getElementById("service-select")
+        const typeWrapper = document.getElementById("type-select"),
+            serviceWrapper = document.getElementById("service-select")
 
-    initCustomSelect(typeWrapper);
-    initCustomSelect(serviceWrapper);
+        initCustomSelect(typeWrapper);
+        initCustomSelect(serviceWrapper);
 
-    //form for doctors and document
-    const typeSelect = typeWrapper.querySelector("select"),
-        serviceSelect = serviceWrapper.querySelector("select"),
-        serviceOptions = serviceWrapper.querySelectorAll(".custom-option"),
-        serviceSelected = serviceWrapper.querySelector(".custom-selected")
 
-    function filterServices(type, preferredValue) {
-        const realOptions = [...serviceSelect.querySelectorAll("option")],
-            label = document.querySelector(".services_form label")
-        let firstVisible = null
+        //form for doctors and document
+        const typeSelect = typeWrapper.querySelector("select"),
+            serviceSelect = serviceWrapper.querySelector("select"),
+            serviceOptions = serviceWrapper.querySelectorAll(".custom-option"),
+            serviceSelected = serviceWrapper.querySelector(".custom-selected")
 
-        realOptions.forEach((opt, idx) => {
-            const relatedCustom = serviceOptions[idx]
-            if (opt.dataset.type === type) {
-                opt.hidden = false;
-                if (relatedCustom) relatedCustom.style.display = "block"
-                if (!firstVisible) firstVisible = {
-                    opt,
-                    relatedCustom
+        function filterServices(type, preferredValue) {
+            const realOptions = [...serviceSelect.querySelectorAll("option")],
+                label = document.querySelector(".services_form label")
+            let firstVisible = null
+
+            realOptions.forEach((opt, idx) => {
+                const relatedCustom = serviceOptions[idx]
+                if (opt.dataset.type === type) {
+                    opt.hidden = false;
+                    if (relatedCustom) relatedCustom.style.display = "block"
+                    if (!firstVisible) firstVisible = {
+                        opt,
+                        relatedCustom
+                    }
+                } else {
+                    opt.hidden = true;
+                    if (relatedCustom) relatedCustom.style.display = "none"
                 }
-            } else {
-                opt.hidden = true;
-                if (relatedCustom) relatedCustom.style.display = "none"
-            }
-        })
-
-        if (firstVisible) {
-            let selectedOpt = firstVisible.opt
-
-            if (preferredValue) {
-                const pref = realOptions.find(opt => !opt.hidden && opt.value === preferredValue)
-                if (pref) selectedOpt = pref
-            }
-
-            serviceSelect.value = selectedOpt.value
-            serviceSelected.textContent = selectedOpt.textContent
-
-            serviceOptions.forEach(div => div.classList.remove('selected'))
-            const match = [...serviceOptions].find(div => div.dataset.value === selectedOpt.value)
-            if (match) match.classList.add('selected')
-
-            if (selectedOpt.dataset.type === "Запис до лікаря") label.textContent = "Оберіть лікаря"
-            else if (selectedOpt.dataset.type === "Медична довідка") label.textContent = "Оберіть тип довідки"
-        }
-
-    }
-
-    typeSelect.addEventListener("change", () => {
-        filterServices(typeSelect.value)
-    })
-
-    filterServices(typeSelect.value)
-
-    document.querySelectorAll('.doctors .cta').forEach(btn => {
-        btn.addEventListener('click', e => {
-            e.preventDefault()
-            const doctorValue = btn.dataset.doctor,
-                option = [...serviceSelect.options].find(opt => opt.value === doctorValue)
-
-            typeSelect.value = option.dataset.type
-            typeWrapper.querySelector('.custom-selected').textContent = option.dataset.type
-
-            filterServices(option.dataset.type, option.value)
-
-            document.querySelector('#form').scrollIntoView({
-                behavior: 'smooth',
-                block: 'center'
             })
 
-            serviceWrapper.querySelector('.custom-select').classList.remove('open')
+            if (firstVisible) {
+                let selectedOpt = firstVisible.opt
+
+                if (preferredValue) {
+                    const pref = realOptions.find(opt => !opt.hidden && opt.value === preferredValue)
+                    if (pref) selectedOpt = pref
+                }
+
+                serviceSelect.value = selectedOpt.value
+                serviceSelected.textContent = selectedOpt.textContent
+
+                serviceOptions.forEach(div => div.classList.remove('selected'))
+                const match = [...serviceOptions].find(div => div.dataset.value === selectedOpt.value)
+                if (match) match.classList.add('selected')
+
+                if (selectedOpt.dataset.type === "Запис до лікаря") label.textContent = "Оберіть лікаря"
+                else if (selectedOpt.dataset.type === "Медична довідка") label.textContent = "Оберіть тип довідки"
+            }
+
+        }
+
+        typeSelect.addEventListener("change", () => {
+            filterServices(typeSelect.value)
         })
 
-    })
+        filterServices(typeSelect.value)
+
+
+        //автозаповнення довідки зі сторінки довідк и
+        const savedService = localStorage.getItem('selectedService');
+
+        if (savedService) {
+            const realOptions = [...serviceSelect.querySelectorAll("option")],
+                matchedOption = realOptions.find(opt => opt.value === savedService)
+
+            if (matchedOption) {
+                typeSelect.value = matchedOption.dataset.type
+                typeWrapper.querySelector('.custom-selected').textContent = matchedOption.dataset.type
+
+                filterServices(matchedOption.dataset.type, matchedOption.value)
+                document.querySelector('#form')?.scrollIntoView({ behavior: 'smooth', block: 'center' })
+
+                localStorage.removeItem('selectedService')
+            }
+        }
+
+        //автозаповнення лікаря
+        document.querySelectorAll('.doctors .cta').forEach(btn => {
+            btn.addEventListener('click', e => {
+                e.preventDefault()
+                const doctorValue = btn.dataset.doctor,
+                    option = [...serviceSelect.options].find(opt => opt.value === doctorValue)
+
+                typeSelect.value = option.dataset.type
+                typeWrapper.querySelector('.custom-selected').textContent = option.dataset.type
+
+                filterServices(option.dataset.type, option.value)
+
+                document.querySelector('#form').scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'center'
+                })
+
+                serviceWrapper.querySelector('.custom-select').classList.remove('open')
+            })
+
+        })
+    }
+
+
+    //запис у локал сторедж опції для селекта на довідки
+    if (document.querySelector('.page_cta[data-service]')) {
+        const ctaButtons = document.querySelectorAll('.page_cta[data-service]');
+
+        ctaButtons.forEach(button => {
+            button.addEventListener('click', function (e) {
+
+                const serviceType = this.getAttribute('data-service');
+
+                if (serviceType) {
+                    localStorage.setItem('selectedService', serviceType)
+                }
+            })
+        })
+    }
+    
 })
--- src/js/validation.js
+++ src/js/validation.js
@@ -1,75 +1,77 @@
-const form = document.querySelector("form[action='sendorder.php']"),
-    phoneInput = form.querySelector("input[name='userPhone']"),
-    nameInput = form.querySelector("input[name='userName']"),
-    errorTel = document.querySelector(".error"),
-    errorName = document.querySelector(".errorName"),
-    maxLength = 30,
-    minLength = 3
+if (document.querySelector("form[action='sendorder.php']")) {
+    const form = document.querySelector("form[action='sendorder.php']"),
+        phoneInput = form.querySelector("input[name='userPhone']"),
+        nameInput = form.querySelector("input[name='userName']"),
+        errorTel = document.querySelector(".error"),
+        errorName = document.querySelector(".errorName"),
+        maxLength = 30,
+        minLength = 3
 
-phoneInput.addEventListener("focus", function () {
-    if (!phoneInput.value.startsWith("+380")) {
-        phoneInput.value = "+380"
+    phoneInput.addEventListener("focus", function () {
+        if (!phoneInput.value.startsWith("+380")) {
+            phoneInput.value = "+380"
+        }
+    })
+
+    phoneInput.addEventListener("input", function () {
+        let inputValue = phoneInput.value,
+            cleanedValue = inputValue.replace(/[^\d+]/g, "")
+
+        if (!cleanedValue.startsWith("+380")) {
+            cleanedValue = "+380" + cleanedValue.slice(3)
+        }
+
+        if (cleanedValue.length > 13) {
+            cleanedValue = cleanedValue.slice(0, 13)
+        }
+
+        phoneInput.value = cleanedValue
+
+        const validInput = isValidPhoneNumber(cleanedValue)
+
+        if (validInput) {
+            phoneInput.style.borderColor = 'green'
+            phoneInput.style.color = '#121212'
+            errorTel.style.display = "none"
+        } else {
+            phoneInput.style.borderColor = '#EB4242'
+            phoneInput.style.color = '#EB4242'
+            errorTel.style.display = "block"
+        }
+    })
+
+
+    form.addEventListener("submit", (e) => {
+        const phone = phoneInput.value.trim(),
+            name = nameInput.value.trim()
+
+        let valid = true
+
+        if (!phone || !isValidPhoneNumber(phone) || phone.length < 13) {
+            errorTel.style.display = "block"
+            phoneInput.style.borderColor = '#EB4242'
+            valid = false
+        } else {
+            errorTel.style.display = "none"
+            phoneInput.style.borderColor = 'green'
+        }
+
+        if (name.length < minLength || name.length > maxLength) {
+            errorName.style.display = "block"
+            nameInput.style.borderColor = '#EB4242'
+            valid = false
+        } else {
+            errorName.style.display = "none"
+            nameInput.style.borderColor = 'green'
+        }
+
+        if (!valid) {
+            console.log("+");
+            e.preventDefault()
+        }
+    })
+
+    function isValidPhoneNumber(phoneNumber) {
+        return /^\+380\d{9}$/.test(phoneNumber)
     }
-})
-
-phoneInput.addEventListener("input", function () {
-    let inputValue = phoneInput.value,
-        cleanedValue = inputValue.replace(/[^\d+]/g, "")
-
-    if (!cleanedValue.startsWith("+380")) {
-        cleanedValue = "+380" + cleanedValue.slice(3)
-    }
-
-    if (cleanedValue.length > 13) {
-        cleanedValue = cleanedValue.slice(0, 13)
-    }
-
-    phoneInput.value = cleanedValue
-
-    const validInput = isValidPhoneNumber(cleanedValue)
-
-    if (validInput) {
-        phoneInput.style.borderColor = 'green'
-        phoneInput.style.color = '#121212'
-        errorTel.style.display = "none"
-    } else {
-        phoneInput.style.borderColor = '#EB4242'
-        phoneInput.style.color = '#EB4242'
-        errorTel.style.display = "block"
-    }
-})
-
-
-form.addEventListener("submit", (e) => {
-    const phone = phoneInput.value.trim(),
-        name = nameInput.value.trim()
-
-    let valid = true
-
-    if (!phone || !isValidPhoneNumber(phone) || phone.length < 13) {
-        errorTel.style.display = "block"
-        phoneInput.style.borderColor = '#EB4242'
-        valid = false
-    } else {
-        errorTel.style.display = "none"
-        phoneInput.style.borderColor = 'green'
-    }
-
-    if (name.length < minLength || name.length > maxLength) {
-        errorName.style.display = "block"
-        nameInput.style.borderColor = '#EB4242'
-        valid = false
-    } else {
-        errorName.style.display = "none"
-        nameInput.style.borderColor = 'green'
-    }
-
-    if (!valid) {
-        console.log("+");
-        e.preventDefault()
-    }
-})
-
-function isValidPhoneNumber(phoneNumber) {
-    return /^\+380\d{9}$/.test(phoneNumber)
 }
